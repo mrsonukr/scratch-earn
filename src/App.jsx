13adf5c87ca1800cac84a31e4e4deff81e47b7e3
mrsonukr <MSSONUKR@GMAIL.COM>
2025-10-07 12:07:07 +0530

    le

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Scratch from "./ui/Scratch";
 import PopUp from "./ui/PopUp";
-import topRaksha from "./assets/top-raksha.png";
-import bottomRaksha from "./assets/bottom-raksha.gif";
+import topRaksha from "./assets/top-raksha.webp";
+import bottomRaksha from "./assets/bottom-raksha.webp";
 
 const App = () => {
   const [isScratchCleared, setIsScratchCleared] = useState(false);
